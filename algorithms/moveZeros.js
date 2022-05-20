f04b8405ae1ba89bd35e14666502d8b44fee54ac
zamrini <karim.e.zamrini@gmail.com>
2022-05-20 15:01:52 -0700

    moveZeroes solution

--- algorithms/moveZeros.js
+++ algorithms/moveZeros.js
@@ -21,6 +21,17 @@ Constraints:
 -231 <= nums[i] <= 231 - 1
 */
 
-var moveZeroes = function(nums) {
-    
+var moveZeroes = function (nums) {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+
+  for (var i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
 };
